28218fc5e3faaab7de0a88fa6dbd0c5fabf85bad
root <prasad.patil49@gmail.com>
2021-05-20 12:05:47 -0400

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
 from tabulate import tabulate
+import os
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+
+app.config['MYSQL_HOST'] = os.environ['MYSQL_SERVICE_PORT_3306_TCP_ADDR']
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_ROOT_PASSWORD']
+app.config['MYSQL_DB'] = 'sakila'
+
+mysql = MySQL(app)
+
+cursor = mysql.connection.cursor()
+cursor.execute("show tables")
+data = cursor.fetchone()
+
+print(data)
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
